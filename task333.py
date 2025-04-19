2436d087f5cb60f6e32b2cf29bbab39b6a6942c7
bhattapuspraj <bhattapuspraj4@gmail.com>
2025-04-19 14:53:24 +1200

    Update task333.py

--- task333.py
+++ task333.py
@@ -1,4 +1,6 @@
 from task2 import  ferry_service_total()
+
+#this fuction show the booking status and show approval reference if approved
 def booking_approval():
     status = "Pending"
     print(f"Status: {status}")
@@ -10,10 +12,13 @@ def booking_approval():
     else:
         print("still pending approvel.")
 
-
+#passenger information
 ID_Number, Passenger_Name = customer_booking()
+
+#cost information
 total_cost, items_price = ferry_service_total
 
+#booking information
 print("Enter the information of booking approvel:")
 print(f"Passenger Name: {Passenger_Name}")
 print(f"ID Number: {ID_Number}")
